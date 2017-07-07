7606ee6ffa0fa9bcb6bc1ed7161480cbb8110820
LoveXuliping <3194581092@qq.com>
2017-07-07 14:26:33 +0800

    DaemonThread
    对Daemon线程进行测试，证明当非Daemon线程不存在的时候，虚拟机停止，而Daemon线程马上终止

---
+++ DaemonTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.liyonghui.lock;
+
+/**
+ * @author Administrator
+ *测试Daemon线程的代码，当Java虚拟机退出时，Daemon线程的finally方法不一定会执行
+ */
+public class Daemon {
+
+	/**
+	 * @param args
+	 * 代码中没有任何的输出，证明当虚拟机退出的时候
+	 * Daemon线程马上终止，不会输出finally的内容
+	 */
+	public static void main(String[] args) {
+		// 测试方法的入口
+		Thread thread = new Thread(new DaemonRunner(), "DaemonRunner");
+		thread.setDaemon(true);
+		thread.start();
+
+	}
+	
+	static class DaemonRunner implements Runnable{
+
+		public void run() {
+			//实现run方法
+			try{SleepUtils.second(10);}
+			finally{
+			System.out.println("DaemonThread finally run.");}
+			
+		}
+		
+		
+	}
+
+}
