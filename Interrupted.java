2da4270705eb0181bf50ec1c8c887db3ade7bed2
LoveXuliping <3194581092@qq.com>
2017-07-07 15:08:46 +0800

    对线程中断标识位的测试
    抛出InterruptedException异常之后的线程SleelThread，其中的标识位别清除；而一直忙碌运行的线程BusyThread，中断标识位没有被清除

---
+++ Interrupted.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.liyonghui.lock;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Administrator
+ *对线程中断的中断标识位的测试
+ */
+public class Interrupted {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 不断的进行睡眠
+		Thread sleepThread = new Thread(new SleepRunner(),"SleepThread");
+		sleepThread.setDaemon(true);
+		//busyThread不断的运行
+		Thread busyThread = new Thread(new BusyRunner(),"BusyRunner");
+		busyThread.setDeamon(true);
+		sleepThread.start();
+		busyThread.start();
+		//休眠5秒，让sleepThread和busyThread充分运行
+		TimeUnit.SECONDS.sleep(5);
+		sleepThread.interrupt();
+		busyThread.interrupt();
+		System.out.println("SleepThread interrupted is"+sleepThread.isInterrupted());
+		System.out.println("BusyThread interrupted is"+busyThread.isInterrupted());
+		//防止sleepThread和busyThread即可退出
+		SleepUtils.second(2);
+		}
+	static class SleepRunner implements Runnable{
+
+		public void run() {
+			while (true) {
+				SleepUtils.second(10);
+				
+			}
+		}
+		
+	}
+	static class BusyRunner implements Runnable{
+
+		public void run() {
+			while (true) {
+				
+				
+			}
+			
+		}
+		
+		
+	}
+
+	}
