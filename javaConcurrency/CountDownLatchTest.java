a85e5fc33c5c197aeee6f40c486ed14aeeb06af5
LoveXuliping <3194581092@qq.com>
2017-07-09 15:30:51 +0800

    CountDownLatch类的测试
    利用CountDownLatch工具

---
+++ javaConcurrency/CountDownLatchTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.liyonghi.Atomic;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Administrator
+ *利用CountDownLatch工具
+ */
+public class CountDownLatchTest {
+
+	/**
+	 * @param args
+	 */
+	//实例化一个CountDownLatch类对象
+	static CountDownLatch c = new CountDownLatch(2);//计数器为2
+	public static void main(String[] args) throws InterruptedException {
+		new Thread(new Runnable() {
+			//放在同一个线程里面执行
+			public void run() {
+				System.out.println(1);
+				c.countDown();
+				System.out.println(2);
+				c.countDown();
+				
+				
+			}
+		}).start();
+		c.await();//等待线程的完成
+		System.out.println(3);
+	}
+
+}
