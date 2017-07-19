9905b2f53c61fa3b167c28c3d2fe25873c8ff1b6
LoveXuliping <3194581092@qq.com>
2017-07-19 14:26:39 +0800

    完善说明
    修改的类的变量一定要为可见，要不会出错

--- Atomic/Candidate.java
+++ Atomic/Candidate.java
@@ -31,7 +31,7 @@ public class AtomicIntegerFieldUpdateDemo {
 			t[i].start();
 		}
 		for (int i = 0; i < 10000; i++) {
-			t[i].join();
+			t[i].join();//让当前主线程等待所有线程执行完毕，再往下面执行
 		}
 		System.out.println("score="+stu.score);
 		System.out.println("allScore="+allScore);
